39aab29f40ce0e1fc7b39300dcfc66872f891469
Bing-Jing <jeff20001234@yahoo.com.tw>
2020-06-01 20:46:50 +0800

    add es_hidden

--- code/PytorchCNN/mlp.py
+++ code/PytorchCNN/mlp.py
@@ -80,27 +80,34 @@ class ResidualBlock(nn.Module):
         return out
 
 class ResNet(nn.Module):
-    def __init__(self, ResidualBlock, numClasses=3):
+    def __init__(self, ResidualBlock, numClasses=3,hp_dic = {
+                                                            "layer1" : 64,
+                                                            "layer2" : 128,
+                                                            "layer3" : 256,
+                                                            "layer4" : 512,
+                                                            "linear1" : 1024,
+                                                            "linear2" : 1024
+                                                            }):
         super(ResNet, self).__init__()
-        self.inchannel = 64
+        self.inchannel = hp_dic["layer1"]
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(3, hp_dic["layer1"], kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
         )
-        self.layer1 = self.make_layer(ResidualBlock, 64,  2, stride=1)
-        self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride=2)
-        self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride=2)
-        self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride=2)
+        self.layer1 = self.make_layer(ResidualBlock, hp_dic["layer1"],  2, stride=1)
+        self.layer2 = self.make_layer(ResidualBlock, hp_dic["layer2"], 2, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, hp_dic["layer3"], 2, stride=2)
+        self.layer4 = self.make_layer(ResidualBlock, hp_dic["layer4"], 2, stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
         self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 1024),
+            nn.Linear( hp_dic["layer4"] * 7 * 7, hp_dic["linear1"]),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            nn.Linear(1024, 1024),
+            nn.Linear(hp_dic["linear1"], hp_dic["linear2"]),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            nn.Linear(1024, numClasses)
+            nn.Linear(hp_dic["linear2"], numClasses)
         )
 
     def make_layer(self, block, channels, num_blocks, stride):
--- code/PytorchCNN/trainMango.py
+++ code/PytorchCNN/trainMango.py
@@ -23,10 +23,10 @@ MU, LAMBDA = 4, 8
 NGEN = 30  # number of generations
 
 IND_SIZE = 6
-MIN_VALUE = 0
-MAX_VALUE = 1
-MIN_STRATEGY = 0.3
-MAX_STRATEGY = 0.8
+MIN_VALUE = 4
+MAX_VALUE = 8192
+MIN_STRATEGY = 2
+MAX_STRATEGY = 1024
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", array.array, typecode="d", fitness=creator.FitnessMax, strategy=None)
@@ -34,9 +34,9 @@ creator.create("Strategy", array.array, typecode="d")
 
 # Individual generator
 def generateES(icls, scls, size, imin, imax, smin, smax):
-    ind = icls([0.4914, 0.4822, 0.4465, 0.2023, 0.1994, 0.2010])
+    ind = icls([64, 128, 256, 512, 1024, 1024])
     # ind = icls(random.uniform(imin, imax) for _ in range(size))
-    ind.strategy = scls(random.uniform(smin, smax) for _ in range(size))
+    ind.strategy = scls(random.randint(smin, smax) for _ in range(size))
     return ind
 
 def checkStrategy(minstrategy):
@@ -53,15 +53,14 @@ def checkStrategy(minstrategy):
 
 # ==========================================
 
-global_a = 0
 
 class train():
-    def __init__(self,classes = ["A","B","C"], max_epoch = 10, lr = 1e-4, batch_size = 32,
+    def __init__(self,classifier_hp_dict = {},classes = ["A","B","C"], max_epoch = 10, lr = 1e-4, batch_size = 16,
                     image_size= 256, validation_frequency = 5, weight_path = "weight", data_path="data"):
         if not os.path.isdir(weight_path):
             os.makedirs(weight_path)
         self.data_path = data_path
-        self.device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.classes = classes
         self.image_size = image_size
         self.validation_frequency = validation_frequency
@@ -69,23 +68,21 @@ class train():
         self.lr = lr
         self.batch_size = batch_size
 
-        self.classifier_net = VGG16_model(numClasses=len(self.classes)).to(self.device)
-        self.classifier_net = ResNet(ResidualBlock=ResidualBlock,numClasses=len(self.classes)).to(self.device)
+        # self.classifier_net = VGG16_model(numClasses=len(self.classes)).to(self.device)
+        self.classifier_net = ResNet(ResidualBlock=ResidualBlock,numClasses=len(self.classes),
+                                        hp_dic=classifier_hp_dict).to(self.device)
         self.optimizer = optim.Adam(self.classifier_net.parameters(), lr=self.lr)
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer,step_size=100,gamma=0.98)
         self.cross = nn.CrossEntropyLoss().to(self.device)
 
-    def run(self, ind):
-        global global_a
-
-        ind = [x % 1 for x in ind]
+    def run(self):
         result = 0.0
 
         dataTransformsTrain = transforms.Compose([
             transforms.RandomResizedCrop(self.image_size, interpolation=2),
             transforms.RandomRotation(degrees=(-180,180)),
             transforms.ToTensor(),
-            transforms.Normalize([ind[0], ind[1], ind[2]], [ind[3], ind[4], ind[5]])
+            transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
             ])
         trainDatasets = Mango_dataset(os.path.join(self.data_path,"train.csv"), os.path.join(self.data_path,"C1-P1_Train"), dataTransformsTrain)
         dataloadersTrain = torch.utils.data.DataLoader(trainDatasets, batch_size=self.batch_size, shuffle=True, num_workers=2)
@@ -110,20 +107,19 @@ class train():
             print("Training loss = {}".format(totalLoss/count))
             print("step = {}, Training Accuracy: {}".format(step,accuracy / count))
             if step % self.validation_frequency == 0 or step == self.max_epoch-1:
-                result = self.validation(ind)
+                result = self.validation()
                 self.store_weight(step)
 
 
-        global_a = train()
         return result,
 
-    def validation(self, ind):
+    def validation(self):
         with torch.no_grad():
             dataTransformsValid = transforms.Compose([
             transforms.Resize(self.image_size),
             transforms.CenterCrop(self.image_size),
             transforms.ToTensor(),
-            transforms.Normalize([ind[0], ind[1], ind[2]], [ind[3], ind[4], ind[5]])
+            transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
             ])
             validDatasets = Mango_dataset(os.path.join(self.data_path,"dev.csv"), os.path.join(self.data_path,"C1-P1_Dev"), dataTransformsValid)
             dataloadersValid = torch.utils.data.DataLoader(validDatasets, batch_size=self.batch_size, shuffle=False)
@@ -160,23 +156,31 @@ class train():
             return self.classes[torch.argmax(outputs)]
 
 
-
+def es_hidden(ind):
+    hp_dic = {
+        "layer1" : int(ind[0]),
+        "layer2" : int(ind[1]),
+        "layer3" : int(ind[2]),
+        "layer4" : int(ind[3]),
+        "linear1" : int(ind[4]),
+        "linear2" : int(ind[5])
+    }
+    a = train(classifier_hp_dict=hp_dic)
+    result = a.run()
+    return result, 
 
 def main():
 
-    global global_a
-
     random.seed(64)
 
-    global_a = train()
-
     toolbox = base.Toolbox()
+    toolbox.register("attr_int", random.randint,1, 65535)
     toolbox.register("individual", generateES, creator.Individual, creator.Strategy, IND_SIZE, MIN_VALUE, MAX_VALUE, MIN_STRATEGY, MAX_STRATEGY)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     toolbox.register("mate", tools.cxESBlend, alpha=0.1)
     toolbox.register("mutate", tools.mutESLogNormal, c=1.0, indpb=0.03)
     toolbox.register("select", tools.selTournament, tournsize=3)
-    toolbox.register("evaluate", global_a.run)
+    toolbox.register("evaluate", es_hidden)
 
     toolbox.decorate("mate", checkStrategy(MIN_STRATEGY))
     toolbox.decorate("mutate", checkStrategy(MIN_STRATEGY))
