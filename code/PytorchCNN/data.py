60c7714e71c7652ff1decb4bac19c0f479711370
Bing-Jing <jeff20001234@yahoo.com.tw>
2020-06-12 23:56:57 +0800

    add eval

--- code/PytorchCNN/data.py
+++ code/PytorchCNN/data.py
@@ -24,3 +24,20 @@ class Mango_dataset(Dataset):
     
     def __class__(self):
         return self.labels
+
+class Eval_dataset(Dataset):
+    def __init__(self, csvFile, data_path, data_transform):
+        self.df = pd.read_csv(csvFile)
+        self.data_path = data_path
+        self.xTrain = self.df['image_id']
+        self.data_transform = data_transform
+
+    def __getitem__(self, index):
+        img = Image.open(os.path.join(self.data_path, self.xTrain[index]))
+        img = img.convert('RGB')
+        if self.data_transform is not None:
+            img = self.data_transform(img)
+        return img, index
+
+    def __len__(self):
+        return len(self.xTrain.index)
---
+++ code/PytorchCNN/eval.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import pandas as pd
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+from mlp import VGG16_model, ResNet, ResidualBlock
+from data import Eval_dataset
+
+def eval(model, imgsize, data_path, device, classes=["A","B","C"]):
+    df = pd.read_csv(os.path.join(data_path,"test.csv"))
+    with torch.no_grad():
+            dataTransformsValid = transforms.Compose([
+            transforms.Resize(imgsize),
+            transforms.CenterCrop(imgsize),
+            transforms.ToTensor(),
+            transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+            ])
+            validDatasets = Eval_dataset(os.path.join(data_path,"test.csv"), os.path.join(data_path,"C1-P1_Test"), dataTransformsValid)
+            dataloadersValid = torch.utils.data.DataLoader(validDatasets, batch_size=1, shuffle=False)
+            model.eval()
+            for x, idx in dataloadersValid:
+                x = x.to(device)
+                outputs = model(x)
+                _, predicted = torch.max(outputs.data, 1)
+                print(classes[np.argmax(predicted)])
+                df["label"][idx] = classes[np.argmax(predicted)]
+
