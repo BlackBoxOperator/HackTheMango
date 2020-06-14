ce0799a2e492392351de1338a954a88fa247f8f7
nobodyzxc <nobodyzxc.tw@gmail.com>
2020-06-14 18:14:33 +0800

    add new esPipeline

--- code/PytorchCNN/esPipeline.py
+++ code/PytorchCNN/esPipeline.py
@@ -3,11 +3,15 @@ import albumentations as A
 from albumentations.pytorch import ToTensor
 from pprint import pprint
 
+fixed = lambda x: x
+paried = lambda x: (x, x)
+sorted_tuple = lambda x: tuple(sorted(x))
 flatten = lambda x: [y for l in x for y in flatten(l)] if type(x) is list else [x]
 
 # can we use IAA series here?
 
-pipeset = [
+"""
+pipes_default = [
     [ # uint8 only
         # channe
         A.InvertImg(always_apply=False, p=0.5),
@@ -103,19 +107,320 @@ pipeset = [
     #],
  
 ]
-
-flattend_pipeset = flatten(pipeset) 
-
-def numOfPipeline():
-    return len(pipeset) + sum([len(p) for p in pipeset]) * 2
+"""
 
 pipeline_length = 12
 # pipeline_length = numOfPipeline() # full pipeline
 nature_beg = 2
 normal_beg = 3
-nature_index = sum([len(x) for x in pipeset[:nature_beg]])
-normalize_index = sum([len(x) for x in pipeset[:normal_beg]])
-bc_start = sum([len(x) for x in pipeset[:normal_beg + 1]])
+
+def CenterCrop(height=128, width=128):
+    return A.Compose(
+            [A.CenterCrop(height, width, always_apply=False, p=1.0),
+            A.Resize(height=256, width=256, interpolation=1, always_apply=False, p=1)], p = 1)
+
+CenterCrop.get_class_fullname = lambda: 'albumentations.augmentations.transforms.CenterCrop'
+
+pipes = [
+    [ # uint8 only
+        # channel
+        A.InvertImg,
+        A.Posterize,
+        # normalize, CLAHE = histogram equalize
+        A.CLAHE,
+        A.Equalize,
+        # noise
+        A.ISONoise,
+    ],
+
+    [ # blur
+        A.Blur,
+        A.MotionBlur,
+        A.MedianBlur,
+        A.GaussianBlur,
+        A.RandomGamma,
+    ],
+
+    [ # nature nature_beg = 2
+        A.RandomSnow,
+        A.RandomRain,
+        A.RandomFog,
+        A.RandomSunFlare,
+        A.RandomShadow,
+    ],
+
+    [ # normalize normal_beg = 3
+        A.Normalize,
+    ],
+
+    [ # 飽和度/色彩相關
+        # will normalize by max or mean brightness
+        A.RandomBrightnessContrast,
+        A.HueSaturationValue,
+        A.RGBShift,
+        A.RandomBrightness,
+        A.RandomContrast,
+        A.ChannelDropout,
+    ],
+
+    [ # affine
+        A.VerticalFlip,
+        A.HorizontalFlip,
+        A.Flip,
+        A.RandomRotate90,
+        A.Rotate,
+        A.ShiftScaleRotate,
+        A.Transpose,
+    ],
+
+    [ # 形變
+        A.OpticalDistortion,
+        A.GridDistortion,
+        A.ElasticTransform,
+    ],
+
+    [ # grid dropout or shuffle
+        A.CoarseDropout,
+        A.Cutout,
+        A.RandomGridShuffle,
+    ],
+
+    [ # noise
+        A.GaussNoise,
+    ], 
+
+    [ # channel
+        A.ChannelShuffle,
+        A.ToGray,
+        A.Solarize,
+    ],
+
+    #[ # compression
+    #    A.JpegCompression(quality_lower=99, quality_upper=100, always_apply=False, p=0.5),
+    #    A.ImageCompression(quality_lower=99, quality_upper=100, always_apply=False, p=0.5),
+    #    # compression_type=A.ImageCompressionType.JPEG, 
+    #],
+
+    [ # scale and crop
+        CenterCrop,
+
+        # hand adjust here
+        A.RandomResizedCrop,
+    ],
+
+    #[ # bbox
+    #    A.RandomSizedBBoxSafeCrop(height=256, width=256, erosion_rate=0.0, interpolation=1, always_apply=False, p=1.0),
+    #],
+]
+
+pipes_name = [ {p.get_class_fullname(): 0 for p in group } for group in pipes ]
+# pprint(pipeattr)
+
+pipes_attr =  {
+    'albumentations.augmentations.transforms.CLAHE': {
+      'p': [float, (0.0, 1.0), 0.5,  fixed],
+      'clip_limit': [float, (0.005, 8.0), 4.0, fixed],
+      'tile_grid_size': [int, (3, 25), 8, paried],
+    },
+    'albumentations.augmentations.transforms.Equalize': {
+      'p': [float, (0.0, 1.0), 0.5,  fixed],
+    },
+    'albumentations.augmentations.transforms.ISONoise': {
+      'p': [float, (0.0, 1.0), 0.5,  fixed],
+      'color_shift': [(float, float), ((0.0, 1.0), (0.0, 1.0)), (0.01, 0.05), sorted_tuple],
+      'intensity': [(float, float), ((0.0, 1.0), 0.0, 1.0), (0.1, 0.5), fixed],
+    },
+    'albumentations.augmentations.transforms.InvertImg': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.Posterize': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'num_bits': [int, (0, 8), 4, fixed],
+    },
+    'albumentations.augmentations.transforms.Blur': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'blur_limit': [int, (3, 100), 7, fixed],
+    },
+    'albumentations.augmentations.transforms.GaussianBlur': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'blur_limit': [int, (3, 100), 7, fixed],
+    },
+    'albumentations.augmentations.transforms.MedianBlur': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'blur_limit': [int, (3, 100), 5, fixed],
+    },
+    'albumentations.augmentations.transforms.MotionBlur': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'blur_limit': [int, (3, 100), 7, fixed],
+    },
+    'albumentations.augmentations.transforms.RandomGamma': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'gamma_limit': [(float, float), ((-130, 130), (-130, 130)), (80, 120), sorted_tuple],
+    },
+    'albumentations.augmentations.transforms.RandomFog': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.RandomRain': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.RandomShadow': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.RandomSnow': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.RandomSunFlare': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.Normalize': {
+      'p': [float, (0.0, 1.0), 1, fixed],
+      'mean': [(float, float, float),
+               ((0.0, 1.0),(0.0, 1.0),(0.0, 1.0)), 
+               (0.485, 0.456, 0.406), fixed],
+      'std': [(float, float, float),
+               ((0.0, 1.0),(0.0, 1.0),(0.0, 1.0)), 
+               (0.229, 0.224, 0.225), fixed],
+    },
+    'albumentations.augmentations.transforms.ChannelDropout': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.HueSaturationValue': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'hue_shift_limit': [int, (5, 50), 20, fixed],
+      'sat_shift_limit': [int, (5, 50), 30, fixed],
+      'val_shift_limit': [int, (5, 50), 20, fixed],
+    },
+    'albumentations.augmentations.transforms.RGBShift': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'r_shift_limit': [int, (5, 50), 20, fixed], 
+      'g_shift_limit': [int, (5, 50), 20, fixed],
+      'b_shift_limit': [int, (5, 50), 20, fixed],
+    },
+    'albumentations.augmentations.transforms.RandomBrightness': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'limit': [float, (0.0, 1.0), 0.2, fixed],
+    },
+    'albumentations.augmentations.transforms.RandomBrightnessContrast': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'brightness_limit': [float, (0.0, 1.0), 0.2, fixed],
+      'contrast_limit': [float, (0.0, 1.0), 0.2, fixed],
+      'brightness_by_max': [int, (0, 1), 1, bool],
+    },
+    'albumentations.augmentations.transforms.RandomContrast': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'limit': [float, (0.0, 1.0), 0.2, fixed],
+    },
+    'albumentations.augmentations.transforms.Flip': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.HorizontalFlip': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.RandomRotate90': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.Rotate': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'limit': [int, (0, 180), 90, fixed],
+      'interpolation': [int, (0, 4), 1, fixed],
+    },
+    'albumentations.augmentations.transforms.ShiftScaleRotate': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'shift_limit': [float, (0.0, 1.0), 0.0625, fixed],
+      'scale_limit': [float, (0.0, 1.0), 0.1, fixed],
+      'rotate_limit': [int, (0, 180), 45, fixed],
+      'interpolation': [int, (0, 4), 1, fixed],
+    },
+    'albumentations.augmentations.transforms.Transpose': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.VerticalFlip': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.ElasticTransform': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'alpha': [float, (0, 5), 1, fixed],
+      'sigma': [float, (25, 75), 50, fixed],
+      'alpha_affine': [float, (25, 75), 50, fixed],
+      'interpolation': [int, (0, 4), 1, fixed],
+    },
+    'albumentations.augmentations.transforms.GridDistortion': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'num_steps': [int, (3, 9), 5, fixed],
+      'distort_limit': [float, (0, 1), 0.3, fixed],
+      'interpolation': [int, (0, 4), 1, fixed],
+    },
+    'albumentations.augmentations.transforms.OpticalDistortion': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'distort_limit': [float, (0.0, 1.0), 0.05, fixed],
+      'shift_limit': [float, (0.0, 1.0), 0.05, fixed],
+      'interpolation': [int, (0, 4), 1, fixed],
+    },
+    'albumentations.augmentations.transforms.CoarseDropout': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'max_holes':  [int, (1, 16), 8, fixed], 
+      'max_height': [int, (1, 16), 8, fixed],
+      'max_width':  [int, (1, 16), 8, fixed],
+    },
+    'albumentations.augmentations.transforms.Cutout': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'num_holes':  [int, (1, 16), 8, fixed],
+      'max_h_size': [int, (1, 16), 8, fixed],
+      'max_w_size': [int, (1, 16), 8, fixed],
+    },
+    'albumentations.augmentations.transforms.RandomGridShuffle': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'grid': [int, (1, 64), 3, paried],
+    },
+    'albumentations.augmentations.transforms.GaussNoise': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.ChannelShuffle': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'var_limit': [(float, float), ((5, 60), (5, 60)), (10.0, 50.0), fixed],
+      'mean': [float, (0.0, 1.0), 0.0, fixed],
+    },
+    'albumentations.augmentations.transforms.Solarize': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'threshold': [int, (0, 255), 128, fixed],
+    },
+    'albumentations.augmentations.transforms.ToGray': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+    },
+    'albumentations.augmentations.transforms.RandomResizedCrop': {
+      'p': [float, (0.0, 1.0), 0.5, fixed],
+      'height': [int, (256, 256), 256, fixed],
+      'width':  [int, (256, 256), 256, fixed],
+    },
+    'albumentations.augmentations.transforms.CenterCrop': {
+      'p': [float, (0.0, 1.0), 1, fixed],
+      'height': [int, (64, 240), 128, fixed],
+      'width': [int, (64, 240), 128, fixed],
+    },
+}
+
+def get_init_val(attr, dic):
+    [typ, rang, init, ret] = dic[attr]
+    return ret(init)
+
+def get_pipe_attr(p):
+    return p({
+        attr: get_init_val(attr, pipes_attr)
+        for attr in pipes_attr[p.get_class_fullname()]
+    })
+
+#flattend_pipes_default = flatten(pipes_default) 
+flattend_pipes = flatten(pipes) 
+
+nature_index = sum([len(x) for x in pipes[:nature_beg]])
+normalize_index = sum([len(x) for x in pipes[:normal_beg]])
+bc_start = sum([len(x) for x in pipes[:normal_beg + 1]])
+
+single_pipes = sum([len(p) for p in pipes]) * 2
+group_pipes = len(pipes)
+
+def numOfPipeline():
+    return len(pipes) + sum([len(p) for p in pipes]) * 2
 
 def idxList2trainPipeline(index_list):
 
@@ -124,8 +429,6 @@ def idxList2trainPipeline(index_list):
     post_add = []
     g_post_add = []
     pipeline = []
-    single_pipes = sum([len(p) for p in pipeset]) * 2
-    group_pipes = len(pipeset)
 
     for idx in index_list:
         if idx >= single_pipes:
@@ -133,23 +436,44 @@ def idxList2trainPipeline(index_list):
             if group_idx == normal_beg:
                 g_post_add.append(group_idx)
             elif group_idx < normal_beg:
-                pipeline.insert(0, A.OneOf(pipeset[group_idx], p = 0.3))
+                pipeline.insert(0, A.OneOf(
+                    #pipes_default[group_idx],
+                    [p(**get_pipe_attr(p)) for p in pipes[group_idx]],
+                    p = 0.3))
             else:
-                pipeline.append(A.OneOf(pipeset[group_idx], p = 0.3))
+                pipeline.append(A.OneOf(
+                    #pipes_default[group_idx],
+                    [p(**get_pipe_attr(p)) for p in pipes[group_idx]],
+                    p = 0.3))
         elif idx % 2 == 0:
             single_idx = idx // 2
             if single_idx > normalize_index:
-                pipeline.append(flattend_pipeset[idx // 2])
+                p = flattend_pipes[idx // 2]
+                pipeline.append(
+                    #flattend_pipes_default[idx // 2]
+                    p(**get_pipe_attr(p))
+                )
             elif single_idx == normalize_index:
                 post_add.append(normalize_index)
             else:
-                pipeline.insert(0, flattend_pipeset[idx // 2])
+                p = flattend_pipes[idx // 2]
+                pipeline.insert(0,
+                    #flattend_pipes_default[idx // 2]
+                    p(**get_pipe_attr(p))
+                )
 
     for idx in post_add:
-        pipeline.append(flattend_pipeset[idx])
+        p = flattend_pipes[idx]
+        pipeline.append(
+            #flattend_pipes_default[idx]
+            p(**get_pipe_attr(p))
+        )
 
     for idx in g_post_add:
-        pipeline.append(A.OneOf(pipeset[idx], p = 0.3))
+        pipeline.append(A.OneOf(
+            #pipes_default[idx]
+            [p(**get_pipe_attr(p)) for p in pipes[idx]],
+            p = 0.3))
 
     pipeline.insert(0, A.Resize(height=256, width=256, interpolation=1, always_apply=False, p=1))
     pipeline.append(A.Resize(height=128, width=128, interpolation=1, always_apply=False, p=1))
