cde20bae331ee80e2b3e955e5628d81a6bd6c795
nobodyzxc <nobodyzxc.tw@gmail.com>
2020-11-03 04:42:13 +0800

    update scripts

--- code/VisionTransformer/train.py
+++ code/VisionTransformer/train.py
@@ -23,6 +23,8 @@ parser.add_argument('--test-batch-size', type=int, default=64, metavar='N',
                     help='input batch size for testing (default: 64)')
 parser.add_argument('--epochs', type=int, default=100, metavar='N',
                     help='number of epochs to train (default: 100)')
+parser.add_argument('--classes', type=int, default=3, metavar='N',
+                    help='number of classes (default: 3)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
@@ -31,16 +33,45 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
+parser.add_argument('--finetune', type=int, default=1, metavar='S',
+                    help='finetune type (default: 1)')
+parser.add_argument('--augment', type=int, default=2, metavar='S',
+                    help='augment type (default: 2)')
 parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                     help='how many batches to wait before logging training status')
-parser.add_argument('--model-name', type=str, default='polynet', metavar='M',
-                    help='model name (default: polynet)')
+parser.add_argument('--model-name', type=str, default='vit_large_patch32_384', metavar='M',
+                    help='model name (default: vit_large_patch32_384)')
 parser.add_argument('--dropout-p', type=float, default=0.2, metavar='D',
                     help='Dropout probability (default: 0.2)')
+parser.add_argument('--dataset', type=str, default='c1p1', metavar='M',
+                    help='dataset (default: c1p1)')
+parser.add_argument('--freeze', action='store_true',
+                    help='freeze the main network')
 
 args = parser.parse_args()
 use_cuda = not args.no_cuda and torch.cuda.is_available()
 device = torch.device('cuda' if use_cuda else 'cpu')
+
+if args.dataset == 'c1p1':
+    DEV_CSV = "dev.csv"
+    DEV_DIR = "C1-P1_Dev"
+    TRAIN_CSV = "train.csv"
+    TRAIN_DIR = "C1-P1_Train"
+elif args.dataset == 'c1p2':
+    DEV_CSV = "dev.csv"
+    DEV_DIR = "Dev"
+    TRAIN_CSV = "train.csv"
+    TRAIN_DIR = "Train"
+else:
+    print("dataset not supported yet")
+    exit(0)
+
+if args.freeze:
+    print("freeze the main network")
+    FreezePretrained = True
+else:
+    print("tuning all network")
+    FreezePretrained = False
 
 
 def train(model, epoch, optimizer, train_loader, criterion=nn.CrossEntropyLoss()):
@@ -79,8 +110,11 @@ def test(model, test_loader, criterion=nn.CrossEntropyLoss()):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+def size_by_name(name, default = 256):
+    beg = name.rfind('_') + 1
+    return int(name[beg:]) if beg else default
 
-def main(data_path=os.path.join('..', '..', 'c1p1')):
+def main(data_path=os.path.join('..', '..', args.dataset)):
     '''Main function to run code in this script'''
 
     model_name = args.model_name
@@ -88,47 +122,65 @@ def main(data_path=os.path.join('..', '..', 'c1p1')):
     if model_name == 'alexnet':
         raise ValueError('The input size of the CIFAR-10 data set (32x32) is too small for AlexNet')
 
-    """
-    classes = (
-        'plane', 'car', 'bird', 'cat', 'deer',
-        'dog', 'frog', 'horse', 'ship', 'truck'
-    )
-    """
-
-    classes = ('A', 'B', 'C')
-
-    """
-     'vit_base_patch16_224',
-     'vit_base_patch16_384',
-     'vit_base_patch32_384',
-     'vit_large_patch16_224',
-     'vit_large_patch16_384',
-     'vit_large_patch32_384',
-     'vit_small_patch16_224',
-    """
-
     model = timm.create_model(
-            'vit_small_patch16_224',
+            model_name,
             pretrained=True,
-            num_classes=len(classes),
+            num_classes=args.classes,
             drop_rate=args.dropout_p,
             # no input_size in this package
             # input_size=(32, 32) if model_name.startswith(('vgg', 'squeezenet')) else None,
             )
 
+    if FreezePretrained:
+        for param in model.parameters():
+            param.requires_grad = False
+
+    if args.finetune:
+        if args.finetune == 1:
+            model.head = nn.Sequential(
+                    nn.Linear(model.head.in_features, 2048),
+                    nn.ReLU(),
+                    nn.Linear(2048, 2048),
+                    nn.ReLU(),
+                    nn.Linear(2048, model.head.out_features)
+                )
+        else:
+            print("finetune type not supported yet")
+            exit(0)
+
+
     model = model.to(device)
 
-    transform = transforms.Compose([
-        transforms.Scale(224),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean = [0.485, 0.456, 0.406],
-             std = [0.229, 0.224, 0.225]
-            #mean=model.original_model_info.mean,
-            #std=model.original_model_info.std
-            ),
-    ])
+    if args.augment == 0:
+        transform = transforms.Compose([
+                transforms.Scale(224),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=model.original_model_info.mean,
+                    std=model.original_model_info.std),
+            ])
+    elif args.augment == 1:
+        transform = transforms.Compose([
+                transforms.RandomResizedCrop(384, interpolation=2),
+                transforms.RandomRotation(degrees=(-180,180)),
+                transforms.ToTensor(),
+                transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]),
+            ])
+    elif args.augment == 2:
+        transform = transforms.Compose([
+            transforms.RandomRotation(degrees=(-180,180)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            #transforms.RandomResizedCrop(384, interpolation=2),
+            #transform.ColorJitter(),
+            transforms.Resize((size_by_name(model_name), size_by_name(model_name))),
+            transforms.ToTensor(),
+            transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]),
+        ])
+    else:
+        print('augmentation type not supported yet')
+        exit(0)
 
     """
     train_set = torchvision.datasets.CIFAR10(
@@ -140,8 +192,8 @@ def main(data_path=os.path.join('..', '..', 'c1p1')):
     """
 
     train_set = Mango_dataset(
-            os.path.join(data_path,"train.csv"),
-            os.path.join(data_path,"C1-P1_Train"),
+            os.path.join(data_path,TRAIN_CSV),
+            os.path.join(data_path,TRAIN_DIR),
             transform)
 
     train_loader = torch.utils.data.DataLoader(
@@ -157,8 +209,8 @@ def main(data_path=os.path.join('..', '..', 'c1p1')):
     """
 
     test_set = Mango_dataset(
-            os.path.join(data_path,"dev.csv"),
-            os.path.join(data_path,"C1-P1_Dev"),
+            os.path.join(data_path,DEV_CSV),
+            os.path.join(data_path,DEV_DIR),
             transform)
 
     test_loader = torch.utils.data.DataLoader(
--- code/VisionTransformer/train.sh
+++ code/VisionTransformer/train.sh
@@ -1,1 +1,33 @@
-python train.py --epochs 10
+mkdir -p weight
+
+# prepare env
+#conda install -c pytorch pytorch torchvision cudatoolkit=11
+#conda install pyyaml
+
+#python train.py --model-name vit_small_patch16_224 --epochs 100 --lr 0.01 --augment 0 --finetune 0 | tee raw_vit_small_patch16_224.txt
+
+# ------------------------------------------------------------------------
+
+#python train.py --model-name vit_base_patch32_384 --epochs 100 --lr 0.0001 --augment 1 --finetune 0 | tee raw_vit_base_patch32_384.txt
+
+# ------------------------------------------------------------------------
+
+#python train.py --model-name vit_base_patch32_384 --epochs 100 --lr 0.0001 --augment 1 --finetune 1 | tee vit_base_patch32_384.txt
+
+# ------------------------------------------------------------------------
+
+# most proper
+#python train.py --model-name vit_large_patch32_384 --epochs 100 --lr 0.0001 --batch-size 16 --augment 2 --finetune 1 | tee vit_large_patch32_384.txt
+
+# ------------------------------------------------------------------------
+
+# time wasting, overfitting, 48%
+#python train.py --model-name vit_large_patch16_384 --epochs 100 --lr 0.0001 --batch-size 2 --augment 2 --finetune 1 | tee vit_large_patch16_384.txt
+
+# ------------------------------------------------------------------------
+
+python train.py --model-name vit_large_patch16_224 --epochs 100 --lr 0.0001 --batch-size 8 --augment 2 --finetune 1 | tee vit_large_patch16_224.txt
+
+# pretrain not tried yet
+# vit_base_patch16_224
+# vit_base_patch16_384
